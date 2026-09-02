22f3c4355237268d2993c670e39dcdad4f58bb03
Lochana Marasinghe <lochanatharanganie@gmail.com>
2026-09-01 18:55:32 -0700

    updated paths

--- paths.py
+++ paths.py
@@ -8,20 +8,20 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # YEAR = 2024
-# RAW_IMG_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2024_field_season_wheat\\for_img_processing\\2024_images"
-# CORRECTED_RF_IMG_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2024_field_season_wheat\\for_img_processing\\corrected_images_reflectance_value_new"
-# CORRECTED_DN_IMG_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2024_field_season_wheat\\for_img_processing\\corrected_images_digital_number_new"
+# RAW_IMG_DIR = ""
+# CORRECTED_RF_IMG_DIR = ""
+# CORRECTED_DN_IMG_DIR = ""
 #
-# ALIGNED_RGB_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2024_field_season_wheat\\for_img_processing\\aligned_rgb"
+# ALIGNED_RGB_DIR = ""
 #
 # PANEL_DETECT_CSV_OUTPUT = os.path.join(PROJECT_ROOT, 'data\\panel_detection_output\\csv_outputs_2024')
 # PANEL_DETECT_IMG_OUTPUT = os.path.join(PROJECT_ROOT, 'data\\panel_detection_output\\img_outputs_2024')
 
 
 YEAR = 2025
-RAW_IMG_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2025_field_season_wheat\\IoT_data_2025\\for_img_processing\\2025_images"
-CORRECTED_RF_IMG_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2025_field_season_wheat\\IoT_data_2025\\for_img_processing\\corrected_images_reflectance_value_new"
-CORRECTED_DN_IMG_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2025_field_season_wheat\\IoT_data_2025\\for_img_processing\\corrected_images_digital_number_new"
-ALIGNED_RGB_DIR = "C:\\Users\\lochana.marasingha\\Documents\\Wheat Project\\2025_field_season_wheat\\IoT_data_2025\\for_img_processing\\aligned_rgb"
+RAW_IMG_DIR = ""
+CORRECTED_RF_IMG_DIR = ""
+CORRECTED_DN_IMG_DIR = ""
+ALIGNED_RGB_DIR = ""
 PANEL_DETECT_CSV_OUTPUT = os.path.join(PROJECT_ROOT, 'data\\panel_detection_output\\csv_outputs_2025')
 PANEL_DETECT_IMG_OUTPUT = os.path.join(PROJECT_ROOT, 'data\\panel_detection_output\\img_outputs_2025')
