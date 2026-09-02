6bdc4492dfa0cc8131ceaace6615abb64e355acb
Lochana Marasinghe <lochanatharanganie@gmail.com>
2026-09-01 18:48:15 -0700

    onnx convertion description updated

--- model/panel/quantize.py
+++ model/panel/quantize.py
@@ -1,14 +1,16 @@
 """
 @author: Lochana Marasinghe
 @date: 2/27/2026
-@description: 
+@description:
+ .pt to onnx conversion command:
+ yolo export model=model/panel/yolo12s_custom_panel_detection_combined.pt format=onnx  device=CPU simplify=True
+
+ then do preprocess before quantization
+ python -m onnxruntime.quantization.preprocess --input model/panel/yolo12s_custom_panel_detection_combined.onnx --output model/panel/yolo12s_custom_panel_detection_combined_preprocessed.onnx
+
 """
 from onnxruntime.quantization import quantize_dynamic, QuantType
 
-# .pt to onnx conversion command:
-# yolo export model=model/panel/yolo12s_custom_panel_detection_combined.pt format=onnx  device=CPU simplify=True
-# then do preprocess before quantization
-# python -m onnxruntime.quantization.preprocess --input model/panel/yolo12s_custom_panel_detection_combined.onnx --output model/panel/yolo12s_custom_panel_detection_combined_preprocessed.onnx
 
 input_model = "yolo12s_custom_panel_detection_combined_preprocessed.onnx"
 output_model = "yolo12s_custom_panel_detection_combined_int8.onnx"
